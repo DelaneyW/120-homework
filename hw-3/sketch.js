5ab80f17c83cfe539a59b2a3d963ba5af9792896
DelaneyW <delaney.wascherol@umontana.edu>
2018-03-01 20:01:47 -0700

    Week 3 Sketch
    Creation and editing.

---
+++ hw-3/sketch.js
@@ -0,0 +1,9 @@
+// Declare setup function
+function setup() {
+
+    // created a canvas 1000px w and 400px h
+    createCanvas( 1000, 400 );
+
+    // make the background 'lightblue'
+    background( 'lightblue' );
+}
