9cd1880c7c547c5a0fb795202308748dc3d5334f
DelaneyW <delaney.wascherol@umontana.edu>
2018-03-02 17:02:23 -0700

    Edited sketch added text

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,9 +1,19 @@
-// Declare setup function
+
+// Declare the setup function
 function setup() {
 
-    // created a canvas 1000px w and 400px h
-    createCanvas( 1000, 400 );
+    // create a canvas 600px wide and 400px high
+    createCanvas( 800, 400 );
 
-    // make the background 'lightblue'
+      // make the background 'lightblue'
     background( 'lightblue' );
+}
+
+
+//Setting up draw function
+function draw()
+
+     //Creating text line in p5 with string "Hello world!"
+    // This is going to be positioned at x300, y200 ).
+text( "Hello world!", 300, 200 );
 }
