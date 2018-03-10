8593873e72e6e92df28d99b73e4d9ab10bf9a662
DelaneyW <delaney.wascherol@umontana.edu>
2018-03-09 22:02:49 -0700

    Started hw-5 sketch

---
+++ hw-5/sketch.js
@@ -0,0 +1,40 @@
+
+var mvRec;
+mvRec = mvRec + 1;
+
+
+
+  function setup() {
+    //Assigning coShift to shift color in background
+
+
+    //Setting up canvas making it match window w+h
+      createCanvas( 1300, 650 );
+
+  }
+
+  function draw(){
+
+    //coShift = coShift + 1;
+      background( 'rgb(165, 197, 222)');
+      translate( 600,325);
+      // Leaf
+      fill( 'rgb(118, 119, 117)');
+      smooth();
+        scale(1.5)
+      strokeWeight(6)
+      stroke( 'rgb(0, 0, 0)');
+
+
+        arc( 50, 160, 80, 80, 0, QUARTER_PI + mouseX);
+        arc( -50, 160, -80, 80, 0, QUARTER_PI + mouseX);
+
+        ellipse( mouseX, mouseY, -200 );
+        rect( mvRec, 0, 80, 34);
+
+
+
+
+
+
+  }
