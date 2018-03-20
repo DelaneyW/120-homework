adb98d37c63228895ee3f0b707d57cafba9a9b09
DelaneyW <delaney.wascherol@umontana.edu>
2018-03-20 17:40:57 -0600

    hw-6 almost done need to contain

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,18 +1,24 @@
  //let rand1= random( 20, 600);
 //  var coTr = map( mouseX, 0, 1200, 0, 183);
- let pos1 = windowWidth - 80
+  let rcol = 184
+ //let pos1 = windowWidth - 80
+ bcol= 180
+ gcol =160
 
  var moveD = 150
 function setup(){
+
+
+
    createCanvas( windowWidth, 625);
-    background( 'rgb(184 , 105, 105)');
+
 
 }
 
 function draw(){
-
+     rcol = map( mouseX, 0, 1000, 0, 255);
   moveD = moveD + 1;
-
+  background( rcol, gcol, bcol);
   fill( 'white' );
   ellipse( moveD, 10, 200, 70);
 
@@ -22,7 +28,7 @@ function draw(){
     let green = floor( random(150) );
     let blue = floor( random(150) );
   fill( red, green, blue );
-  ellipse( windowWidth/2, windowHeight/2, 90, 90);
+  ellipse( windowWidth/2, 400, 90, 90);
   stroke('white');
 
 
@@ -38,12 +44,12 @@ function draw(){
     var y = i%200;
     line ( 0, i, y , i);}
 
-    translate( 900, 300);
-    scale(7.0);
+    //translate( 900, 300);
+    scale(10.0);
     for (var rl = 0; rl < 100; rl++) {
-    var r = random(50);
-    stroke(r * 5);
-    line(50, rl, 50 + r, rl);
+    var r = random(500);
+    stroke(r * 3);
+    point(r,r);
   }
 
 
