4bf6230e3eb758d33807a92cc7d77ada3a2bfaa9
DelaneyW <delaney.wascherol@umontana.edu>
2018-03-20 14:08:21 -0600

    hw-6
    functional, choppy needs smoothing and to add a few elements before sumbmit

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,5 +1,7 @@
  //let rand1= random( 20, 600);
 //  var coTr = map( mouseX, 0, 1200, 0, 183);
+ let pos1 = windowWidth - 80
+
  var moveD = 150
 function setup(){
    createCanvas( windowWidth, 625);
@@ -19,7 +21,7 @@ function draw(){
   let red = floor( random(150) );
     let green = floor( random(150) );
     let blue = floor( random(150) );
-    //fill( red, green, blue );
+  fill( red, green, blue );
   ellipse( windowWidth/2, windowHeight/2, 90, 90);
   stroke('white');
 
@@ -35,4 +37,14 @@ function draw(){
   for (var i=0; i<width; i++){
     var y = i%200;
     line ( 0, i, y , i);}
+
+    translate( 900, 300);
+    scale(7.0);
+    for (var rl = 0; rl < 100; rl++) {
+    var r = random(50);
+    stroke(r * 5);
+    line(50, rl, 50 + r, rl);
+  }
+
+
 }
