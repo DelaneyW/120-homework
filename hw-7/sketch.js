95d81330e6f20a4675b2ebf5d792642ca49becc7
DelaneyW <delaney.wascherol@umontana.edu>
2018-03-23 18:02:56 -0600

    Color function amost addded

--- hw-7/sketch.js
+++ hw-7/sketch.js
@@ -8,7 +8,11 @@ ball.delta_x = 1;//confine ball variables
 ball.delta_y = 1;
 ball.scale_x = 1;//Scale variables
 ball.scale_y = 1;
-
+ball.color=255;
+ball.stroke= black;
+ball.r = 0
+ball.g = 0
+ball.b = 0
 function setup() {
   createCanvas(windowWidth, 400);//Creating canvas and setting background to white
     background(255);
@@ -17,6 +21,8 @@ function setup() {
 
 
 function draw() {
+
+
  // This links the values of x and y to the delta vars
     ball.x += ball.delta_x * ball.scale_x;
     ball.y += ball.delta_y * ball.scale_y;
@@ -32,14 +38,29 @@ function draw() {
         ball.delta_y = -1 * ball.delta_y;
     }
   //Setting fill and creating ball with variables from above.
-    fill(255);
+    fill(ball.r, ball.g, ball.b);
 
     ellipse(ball.x, ball.y, ball.width, ball.width);
 
+
 }
+
 
 function mousePressed() {
 //This changes the path of the ball when mouse is pressed
     ball.scale_x = map(mouseX, 0, width, 0.5, 10);
     ball.scale_y = map(mouseY, 0, height, 0.5, 10);
+
+    if (ball.x >= width/3){
+    ball.color = ('teal');
+    ball.stroke = ('green');
+
+    }
+    if ( ball.x< width/3 ){
+
+      ball.r = random(25-150);
+      ball.b = random(35-255);
+      ball.g =10;
+
+    }
 }
