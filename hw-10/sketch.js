459b16e6174aec0a4705738ae19956326f0cf6d2
DelaneyW <delaney.wascherol@umontana.edu>
2018-04-12 15:02:51 -0600

    Old sketch edited

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,3 +1,6 @@
+
+///Creating an aray to store values i will use to place circles
+  let array = [300, 500, 700, 900, 1100];
 //declaring red value for bg
   let rcol = 184
 //declaring blue value for bg
@@ -6,26 +9,18 @@
   let gcol =160
 //setting up variable to for movement
   var moveD = 150
-
-//  var nums[50, 30, 80, 100];
-  let xArray = [ 30, 40, 60, 80, 100];
-
+//Setting variables to use for the cloud function
+  let oSz = 10; let oSzY = 200; let oSz_3 = 70;
+//Setting colr variables to use for the background
   var rc = floor( random(150) );
   var gc = floor( random(150) );
   var bc = floor( random(150) );
-  var x = windowWidth/2;
-  var y = 400;
-  var size = 90;
 
-  let numberlist;
 
 function setup(){
 //creating canvas
-
 createCanvas( windowWidth, 625);
 //chose not to setup background here so I could create background effect in draw
-  numberlist = [50, 20, 100, 25, 5, 75];
-
 }
 
 function draw(){
@@ -42,6 +37,10 @@ moveD = moveD + 1;
   let wall3 = 10 ;
   let wall4 = 450;
 
+//creating secound set of contstraints for the cloud
+  let wall5 = windowWidth - 200;
+  let wall6 = windowWidth -300;
+
 //setting up var to constrain white ellipse
   let conO = constrain( moveD, wall3, wall4 );
 
@@ -53,17 +52,25 @@ moveD = moveD + 1;
 //Setting fill to color ellipse white
     fill( 'white' );
 //creating moving white oval
-    ellipse( conO, 10, 200, 70);
+
+    cloud(10, 200, 70);
+///changing constrait of secound cloud
+//creating a smaller cloud
+  conO = constrain( moveD, wall5, wall6 );
+     cloud(200, 100, 80);
+
 
 //creating moving teal circle to be placed below oval
 fill( 'teal');
 ellipse( conE, 46, 55, 55);
 
+//for (var indx = 0; indx < 6; indx++);
 
+for( let i = 0; i< 5; i ++){
+  ellipse( array[i], 400, 90, 90);
+  stroke('white');
 
-    circ(windowWidth/2, 400, 90);
-    circ(windowWidth/4, 500, 70);
-
+}
 
 
 stroke('white');
@@ -82,20 +89,11 @@ for (var i=0; i<width; i++){
   var r = random(500);
   stroke(r * 3);
   point(r,r);
-
-  for (let i = 0; i < numberlist.length; i++) {
-       noFill();
-       stroke(255);
-       ellipse(i * 100 + 100, 100, numberlist[i], numberlist[i]);
-   }
 }
-function circ( x, y, size){
-
-  fill( rc, gc, bc );
-  ellipse( x, y, size, size);
-  stroke('white');
-
+//Naming my own function cloud with the variables set above
+function cloud(oSz, oSzY, oSz_3){
+    ellipse( conO, oSz, oSzY, oSz_3);
+    ellipse(  conO, oSz + 40, oSzY + 100, oSz_3);
 }
 
-
-}
+  }
