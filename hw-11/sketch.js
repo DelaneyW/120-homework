be6da48f5620ec06db7db94d86e8437d33f358a3
DelaneyW <delaney.wascherol@umontana.edu>
2018-04-13 23:28:11 -0600

    Class made kind of
    Error saying Fish not defined

--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -1,4 +1,7 @@
-
+  //Creating object
+//temp got rid of mouse x mouse y crazy biz to test basic function
+  // let fishy = new Fish( mouseX + 10, mouseX + 50, mouseX + 90, mouseX + 25, 'orange');
+  let fish = new Fish(10, 50);
 
 // Declare the setup function
 function setup() {
@@ -13,23 +16,44 @@ function setup() {
   function draw(){
     //Getting rid of stroke
     //noStoke();
-    //Creating a quad shape
-    //quad( 10, 10, 40,40, 10, 40, 10, 40);
-    //Creating a triangle
+    fish.show();
 
-    quad( 10, 10, 50, 50, 50, 10, 10, 50);
-    triangle( 50, 10, 50, 50, 90, 25);
+
+    // quad( 10, 10, 50, 50, 50, 10, 10, 50);
+    // triangle( 50, 10, 50, 50, 90, 25);
   }
+    class Fish {
+  // constuctor(x,y,head_1, head_2,clr){
+    constructor( x, y) {
+      //x = 10, y = 50
+      //head_1 = 90, head_2 = 25
+      this.x = 10;
+      this.y = 50;
+      this.head_1 = 90;
+      this.head_2 = 25;
+      this.color = 'orange';
 
-  class Fish {
-    constuctor(x,y,head,clr){
-      this.x = x;
-      this.y = y;
-      this.head = head
-      this.color = clr;
-      
+        }
+    show() {
+      // push();
+
+        noStroke();
+        //Creating a quad shape
+        //Creating a triangle
+      //Asigning shapes to Fish
+      fill( this.clr );
+      quad( this.x, this.x, this.y, this.y, this.y, this.x, this.x, this.y);
+      //triangle( y, x, y, y, head_1, head_2);
+
+
+
+
+      // pop();
 
     }
 
-
-  }
+    follow() {
+      this.x = mouseX + 10;
+      this.y = mouseY + 50;
+}
+}
